f36a418851b819516b65a2b27569b6d035273742
Alexandre Mota <alexandremot@gmail.com>
2021-03-30 15:16:33 -0300

    versao funcional com puppet e ansible; igonre chaves publicas e privadas

--- DEV/bionic/src/index.php
+++ DEV/bionic/src/index.php
@@ -2,7 +2,19 @@
 <html>
 	<body>
 	<?php
-	echo "Hello World!";
+	echo "Testando conexão <br /><br />";
+	$servername = "192.168.0.23";
+	$username = "phpuser";
+	$password = "pass";
+
+	// create a connection
+	$conn = new mysqli($servername, $username, $password);
+
+	//check connection
+	if ($conn->connect_error){
+		die("conexao falhou: " . $conn->connect_error);
+	}
+	echo "conectado com sucesso!";
 	?> 
 	</body>
 </html>
